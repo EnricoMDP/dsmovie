f2d2afa36d2922c11bc552c996283e579f5b3b0e
Enrico Meira <enrico.depaulameira@gmail.com>
2026-02-16 19:41:04 -0300

    Domain model, database seed

--- backend/src/main/java/com/enrico/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/enrico/dsmovie/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.enrico.dsmovie;
+package com.enrico.dsmovie.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
---
+++ backend/src/main/java/com/enrico/dsmovie/entities/Score.java
@@ -0,0 +1,41 @@
+package com.enrico.dsmovie.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double valor;
+
+    public Score(){
+    }
+
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+
+    public ScorePK getId() {
+        return id;
+    }
+
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+
+    public Double getvalor() {
+        return valor;
+    }
+
+    public void setvalor(Double valor) {
+        this.valor = valor;
+    }
+}
